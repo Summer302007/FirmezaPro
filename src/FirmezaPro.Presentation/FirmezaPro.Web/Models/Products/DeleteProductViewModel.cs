bafd739ef65509587a36e5e6882da19879b3d953
Duana <duanaochoa05@gmail.com>
2026-01-22 22:33:00 -0600

    feat: Created the delete view model, update the edit and delete view

---
+++ src/FirmezaPro.Presentation/FirmezaPro.Web/Models/Products/DeleteProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirmezaPro.Web.Models.Products
+{
+    public class DeleteProductViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
